73ed1c2b78c23f3dc2de796ef300ca75a9ddb436
appu-46 <ap958461@gmail.com>
2023-07-25 00:09:18 +0530

    added home button

--- script.js
+++ script.js
@@ -32,6 +32,28 @@ const renderStats = async function (data) {
     <p class="rapid">Best Rapid rating : ${rapid.best.rating}</p>
   </div>
   </article>`;
+
+  window.onload = function () {
+    var chart = new CanvasJS.Chart('chartContainer', {
+      theme: 'dark2', //light1
+      title: {
+        text: 'Blitz',
+      },
+      data: [
+        {
+          type: 'column',
+          dataPoints: [
+            { label: 'apple', y: 10 },
+            { label: 'orange', y: 15 },
+            { label: 'banana', y: 25 },
+            { label: 'mango', y: 30 },
+            { label: 'grape', y: 28 },
+          ],
+        },
+      ],
+    });
+    chart.render();
+  };
   statsContainer.insertAdjacentHTML('beforeend', html);
   textbox.hidden = true;
   statsButton.hidden = true;
